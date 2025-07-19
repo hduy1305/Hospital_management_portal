2f192c3eec714865899e083f6bdb08936a11744f
hduy1305 <127862604+hduy1305@users.noreply.github.com>
2025-07-19 11:49:09 +0700

    Delete hospital-portal directory

--- hospital-portal/src/App.vue
+++
@@ -1,8 +0,0 @@
-<!-- src/App.vue -->
-<template>
-  <router-view />
-</template>
-
-<script setup>
-// Không cần import HelloWorld nữa
-</script>
--- hospital-portal/src/components/Navbar.vue
+++
@@ -1,7 +0,0 @@
-<!-- src/components/Navbar.vue -->
-<template>
-  <nav class="bg-white px-6 py-3 shadow rounded mb-6 flex justify-between">
-    <div class="font-semibold text-lg">Hospital Portal</div>
-    <div class="text-gray-600">Xin chào, Admin</div>
-  </nav>
-</template>
